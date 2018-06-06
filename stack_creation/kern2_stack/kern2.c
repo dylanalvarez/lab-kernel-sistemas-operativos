270b99651cf414551a83c85e0d294d7fd13d9f69
Dylan <dylanalvarez1995@gmail.com>
2018-06-06 19:41:45 -0300

    WIP

--- stack_creation/kern2_stack/kern2.c
+++ stack_creation/kern2_stack/kern2.c
@@ -3,5 +3,13 @@
 
 void kmain(const multiboot_info_t *mbi) {
     vga_write("kern2 loading.............", 8, 0x70);
+       
+    if (mbi->flags & (1 << 2)) { // bit 2 of flags is set
+        char buf[256] = "cmdline: ";
+        //char *cmdline = (void *) mbi->cmdline;
+        // Aquí usar strlcat() para concatenar cmdline a buf.
+        // ...
+        vga_write(buf, 9, 0x07);
+    }
 }
 
