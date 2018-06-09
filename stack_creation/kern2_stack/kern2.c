ac6d775f6035292950d4d5309b8949994c713792
Dylan <dylanalvarez1995@gmail.com>
2018-06-08 22:51:31 -0300

    Fix boot.S and print parameters to the screen using strlcat

--- stack_creation/kern2_stack/kern2.c
+++ stack_creation/kern2_stack/kern2.c
@@ -1,14 +1,14 @@
 #include "decls.h"
 #include "multiboot.h"
+#include "lib/string.h"
 
 void kmain(const multiboot_info_t *mbi) {
     vga_write("kern2 loading.............", 8, 0x70);
        
     if (mbi->flags & (1 << 2)) { // bit 2 of flags is set
         char buf[256] = "cmdline: ";
-        //char *cmdline = (void *) mbi->cmdline;
         // Aquí usar strlcat() para concatenar cmdline a buf.
-        // ...
+        strlcat(buf, (char *) mbi->cmdline, 256);
         vga_write(buf, 9, 0x07);
     }
 }
